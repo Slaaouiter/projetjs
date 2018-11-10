9477fa6e5b8b85da85ffdc2f17e905d41a162da6
Slaaouiter <40489364+Slaaouiter@users.noreply.github.com>
2018-11-10 20:54:45 +0100

    Add files via upload

---
+++ client/js/script.js
@@ -0,0 +1,105 @@
+window.onload = init;
+
+function init() {
+    new Vue({
+        el: '#app',
+        data: {
+            restaurants: [],
+            input: "",//Pour la recherche
+            page:1,
+            count:0,
+            pagesize:10,
+            cuisine:"",
+            nom:"",
+            ncuisine:"",//new type de cuisine (pour la modification)
+            nnom:"r",//new nom (pour la modification)
+            //Pour la modification
+            visible:{
+                visibility:'hidden',
+            },
+            //
+            pid:0//previous id = Ancient id 
+
+        },
+        mounted() {
+            console.log("--- MOUNTED, appelée avant le rendu de la vue ---");
+            this.getDataFromWebService();
+        },
+        methods: {
+            
+            getDataFromWebService: function () {
+                let url = "http://localhost:8080/api/restaurants?page="+this.page+"&name="+this.input+"&pagesize="+this.pagesize;
+                this.input="";
+                fetch(url).then((data) => {
+                    console.log("les données sont arrivées !")
+                    return data.json();
+                }).then((dataEnJavaScript) => {
+                    // ici on a bien un objet JS
+                    this.restaurants = dataEnJavaScript.data;
+                    this.count=parseInt(dataEnJavaScript.count/this.pagesize);
+                });
+            },
+            addRestaurant: function () {
+                var data= new FormData();
+                data.append("nom",this.nom);
+                data.append("cuisine",this.cuisine);
+                fetch("http://localhost:8080/api/restaurants", { 
+                method: "POST", 
+                body: data 
+                })
+                this.nom="";
+                this.cuisine="";
+                this.getDataFromWebService();
+            },
+            modifierRestaurant: function () {
+                var data= new FormData();
+                data.append("nom",this.nnom);
+                data.append("cuisine",this.ncuisine);
+                fetch("http://localhost:8080/api/restaurants/"+this.pid, { 
+                method: "PUT", 
+                body: data 
+                })
+                this.visible.visibility='hidden';
+                this.getDataFromWebService();
+                
+            },
+            supprimerRestaurant: function (restaurant) {
+                fetch("http://localhost:8080/api/restaurants/"+restaurant._id, { 
+                method: "DELETE", 
+                })
+                this.getDataFromWebService();
+            },
+            rendrevisible:function(restaurant){
+                this.nnom=restaurant.name;
+                this.ncuisine=restaurant.cuisine;
+                this.pid=restaurant._id;
+                this.visible.visibility='visible';
+                
+
+            },
+            getColor: function (index) {
+                return (index % 2) ? 'red' : 'green';
+            },
+            pagePrecedente:function(){
+                if(this.page!=1){
+                this.page--;
+                }
+                else{
+                    this.page=this.count;
+                }
+                this.getDataFromWebService();
+            },
+            pageSuivante:function(){
+                if(this.page!=this.count){
+                    this.page++;
+                    }
+                    else{
+                        this.page=1;
+                    }
+               
+                this.getDataFromWebService();
+            }
+            
+        }
+    })
+}
